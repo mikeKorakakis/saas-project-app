c81f3917baac94b32e1f6ce7aa32117b30ebdde0
Mike Korakakis <michael_1988@windowslive.com>
2018-04-16 12:40:29 -0700

    Complete artifacts

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,9 @@
 class Project < ApplicationRecord
   belongs_to :tenant
   validates_uniqueness_of :title
+  has_many :artifacts, dependent: :destroy
   validate :free_plan_can_only_have_one_project
+
 
   def free_plan_can_only_have_one_project
   	if self.new_record? && (tenant.project.count> 0) && (tenant.plan == 'free')
