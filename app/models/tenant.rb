e93c51a7439be521c7a6db66d4328b96fdf4e9d3
Mike Korakakis <michael_1988@windowslive.com>
2018-04-17 07:14:27 -0700

    add stripe accept payment update tenant

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -3,11 +3,13 @@ class Tenant < ApplicationRecord
    acts_as_universal_and_determines_tenant
   has_many :members, dependent: :destroy
   has_many :projects, dependent: :destroy
+  has_one :payment
+  accepts_nested_attributes_for :payment
 
   def can_create_projects?
     (plan == 'free' && projects.count < 1) || (plan == 'premium')
   end
-  
+
   validates_uniqueness_of :name
   validates_presence_of :name
 
